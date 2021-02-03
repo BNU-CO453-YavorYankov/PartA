b198085021479c39b84a611b3afeab0ce80d910d
yavaka <yavakasolutions@gmail.com>
2021-02-03 22:46:10 +0000

    App01: Add reader class responsible for user input from console

---
+++ ConsoleAppProject/Reader.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleAppProject
+{
+    using System;
+
+    /// <summary>
+    /// Reader class is responsible for reading from the console and return it
+    /// </summary>
+    /// <author>
+    /// Yavor Yankov
+    /// </author>
+    public static class Reader
+    {
+        /// <summary>
+        /// Read from the console a string and parse it to int
+        /// </summary>
+        public static int ReadInteger => int.Parse(Console.ReadLine());
+
+        /// <summary>
+        /// Read from the console a string and parse it to double
+        /// </summary>
+        public static double ReadDouble => double.Parse(Console.ReadLine());
+
+    }
+}
