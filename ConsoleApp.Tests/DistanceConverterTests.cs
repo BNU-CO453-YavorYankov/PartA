201a9113dfb0b23cadd846bc8b9ade53357f0578
yavaka <yavakasolutions@gmail.com>
2021-02-03 17:36:51 +0000

    App01: Added tests for all possible conversions.

--- ConsoleApp.Tests/DistanceConverterTests.cs
+++ ConsoleApp.Tests/DistanceConverterTests.cs
@@ -5,11 +5,102 @@ namespace ConsoleApp.Tests
 {
     public class DistanceConverterTests
     {
+        private DistanceConverter converter;
 
         [SetUp]
         public void Setup()
         {
-            
+            converter = new DistanceConverter();
+        }
+
+        [Test]
+        public void ConvertMilesToFeet()
+        {
+            //Arrange
+            converter.FromUnit = DistanceUnits.Miles;
+            converter.ToUnit = DistanceUnits.Feet;
+            converter.FromDistance = 5;
+
+            //Act
+            converter.ConvertDistance();
+
+            //Assert
+            Assert.That(converter.ToDistance, Is.EqualTo(26400));
+        }
+
+        [Test]
+        public void ConvertMilesToMetres()
+        {
+            //Arrange
+            converter.FromUnit = DistanceUnits.Miles;
+            converter.ToUnit = DistanceUnits.Metres;
+            converter.FromDistance = 2;
+
+            //Act
+            converter.ConvertDistance();
+
+            //Assert
+            Assert.That(converter.ToDistance, Is.EqualTo(3218.69));
+        }
+
+        [Test]
+        public void ConvertFeetToMiles()
+        {
+            //Arrange
+            converter.FromUnit = DistanceUnits.Feet;
+            converter.ToUnit = DistanceUnits.Miles;
+            converter.FromDistance = 12345;
+
+            //Act
+            converter.ConvertDistance();
+
+            //Assert
+            Assert.That(converter.ToDistance, Is.EqualTo(2.34));
+        }
+
+        [Test]
+        public void ConvertFeetToMetres()
+        {
+            //Arrange
+            converter.FromUnit = DistanceUnits.Feet;
+            converter.ToUnit = DistanceUnits.Metres;
+            converter.FromDistance = 258;
+
+            //Act
+            converter.ConvertDistance();
+
+            //Assert
+            Assert.That(converter.ToDistance, Is.EqualTo(78.64));
+        }
+
+        [Test]
+        public void ConvertMetresToMiles()
+        {
+            //Arrange
+            converter.FromUnit = DistanceUnits.Metres;
+            converter.ToUnit = DistanceUnits.Miles;
+            converter.FromDistance = 1695.15;
+
+            //Act
+            converter.ConvertDistance();
+
+            //Assert
+            Assert.That(converter.ToDistance, Is.EqualTo(1.05));
+        }
+
+        [Test]
+        public void ConvertMetresToFeet()
+        {
+            //Arrange
+            converter.FromUnit = DistanceUnits.Metres;
+            converter.ToUnit = DistanceUnits.Feet;
+            converter.FromDistance = 1000;
+
+            //Act
+            converter.ConvertDistance();
+
+            //Assert
+            Assert.That(converter.ToDistance, Is.EqualTo(3280.84));
         }
     }
 }
