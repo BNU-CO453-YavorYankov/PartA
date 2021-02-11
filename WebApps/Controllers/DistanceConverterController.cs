3041511d698dadcde39b8ea5b0db735044d86e1a
yavaka <yavakasolutions@gmail.com>
2021-02-11 22:02:33 +0000

    App01: Web - Add distance converter controller.

---
+++ WebApps/Controllers/DistanceConverterController.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApps.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DistanceConverterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Convert()
+        {
+            return View();
+        }
+    }
+}
