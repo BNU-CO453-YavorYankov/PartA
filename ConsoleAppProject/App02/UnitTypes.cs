5b0f1a06fafd61ae2807ff3345d5113ce6a66861
yavaka <yavakasolutions@gmail.com>
2021-03-02 13:38:05 +0000

    Added unit types enum.

---
+++ ConsoleAppProject/App02/UnitTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleAppProject.App02
+{
+    /// <summary>
+    /// List of unit types
+    /// </summary>
+    /// <author>Yavor Yankov</author>
+    public enum UnitTypes
+    {
+        Imperial = 0,
+        Metric = 1
+    }
+}
