787d06db798b5f760639fbc79e2cf1c767e5172d
yavaka <yavakasolutions@gmail.com>
2021-03-14 14:31:33 +0000

    Added Command abstract class.

---
+++ ConsoleAppProject/App03/Commands/Command.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleAppProject.App03.Commands
+{
+    /// <summary>
+    /// Command abstarct class which is inherited by each concrete command class
+    /// </summary>
+    /// <author>Yavor Yankov</author>
+    public abstract class Command
+    {
+        /// <summary>
+        /// The receiver of the Command Design Pattern.
+        /// StudentGrades class will perform the action from the command
+        /// </summary>
+        protected StudentGrades studentGrades;
+
+        /// <summary>
+        /// The constructor that initialises this command
+        /// and recieve the 'reciever'
+        /// </summary>
+        /// <param name="studentGrades">Reciever of the command</param>
+        public Command(StudentGrades studentGrades)
+            => this.studentGrades = studentGrades;
+        
+        /// <summary>
+        /// Abstract method to be overriden which executes this command
+        /// </summary>
+        public abstract void Execute();
+    }
+}
