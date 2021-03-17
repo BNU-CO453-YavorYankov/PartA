fd518d183865c2a3cf4fa4ac6516c7c1fc2a6cc0
yavaka <yavakasolutions@gmail.com>
2021-03-17 14:48:56 +0000

    Tested creation of new Student.

--- ConsoleApp.Tests/StudentGradesTests.cs
+++ ConsoleApp.Tests/StudentGradesTests.cs
@@ -5,6 +5,7 @@
     using ConsoleAppProject.App03.Commands;
 
     using static ConsoleAppProject.Common.Constants.StudentGrades;
+    using System;
 
     public class StudentGradesTests
     {
@@ -22,9 +23,42 @@
         public void AddStudentCommandShouldAddStudent() 
         {
             var addStudentCommand = new AddStudentCommand(this._studentGrades);
+
             addStudentCommand.Execute();
 
             Assert.That(this._studentGrades.Students.Count, Is.EqualTo(11));
+        }
+
+        [Test]
+        public void CreateStudentWithInvalidNameShouldThrowException() 
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var student = new Student(1, "n", "wrong name");
+
+            }, INVALID_STUDENT_NAME_MSG);
+        }
+
+        [Test]
+        public void AssignNegativeMarkOfStudentShouldThrowException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var student = new Student(1, "Test", "Student");
+                student.Mark = -1;
+
+            }, OUT_OF_RANGE_MARK_MSG);
+        }
+
+        [Test]
+        public void AssignMoreThanMaxMarkOfStudentShouldThrowException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var student = new Student(1, "Test", "Student");
+                student.Mark = 150;
+
+            }, OUT_OF_RANGE_MARK_MSG);
         }
     }
 }
