f283b675e90529ada6e0107512a1bf81e210d80c
yavaka <yavakasolutions@gmail.com>
2021-03-17 14:14:06 +0000

    Added set student id, validation is the new student exists and add student methods.

---
+++ ConsoleApp.Tests/StudentGrades.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp.Tests
+{
+    using NUnit.Framework;
+    using ConsoleAppProject.App03;
+
+    using static ConsoleAppProject.Common.Constants.StudentGrades;
+    
+    public class StudentGrades
+    {
+        private StudentGrades _studentGrades;
+        private StudentGradesInvoker _invoker;
+
+        [SetUp]
+        public void Setup()
+        {
+            this._studentGrades = new StudentGrades(); 
+            this._invoker = new StudentGradesInvoker();
+        }
+    }
+}
