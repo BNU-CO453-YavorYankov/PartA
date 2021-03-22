4852889e45b437afc4fe0bffe993ff79e015a340
yavaka <yavakasolutions@gmail.com>
2021-03-22 23:00:02 +0000

    Added user props and gender enum

--- WebApps/Models/App04/User.cs
+++ WebApps/Models/App04/User.cs
@@ -1,12 +1,23 @@
 ﻿namespace WebApps.Models.App04
 {
+    using System;
     using Microsoft.AspNetCore.Identity;
-    
+
     /// <summary>
     /// The user model keeps the details of this user
     /// </summary>
     public class User : IdentityUser
     {
-        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{this.FirstName} {this.LastName}";
+        public DateTime? DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+    }
+    
+    public enum Gender
+    {
+        male = 0,
+        female = 1
     }
 }
