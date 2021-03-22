4ef4a2b31d44a25e689aad36b3382fe4d736db73
yavaka <yavakasolutions@gmail.com>
2021-03-22 22:58:02 +0000

    Added User model

---
+++ WebApps/Models/App04/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApps.Models.App04
+{
+    using Microsoft.AspNetCore.Identity;
+    
+    /// <summary>
+    /// The user model keeps the details of this user
+    /// </summary>
+    public class User : IdentityUser
+    {
+        
+    }
+}
