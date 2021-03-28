4455f275734c4f6416a76a61fbcb2a24bedf3e97
yavaka <yavakasolutions@gmail.com>
2021-03-28 13:48:55 +0100

    Added post service interface

---
+++ WebApps/Services/Posts/IPostService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using WebApps.Models.App04;
+
+namespace WebApps.Services.Posts
+{
+    public interface IPostService
+    {
+        /// <summary>
+        /// Add new post to the database
+        /// </summary>
+        /// <param name="post">New post</param>
+        Task AddPost(Post post);
+
+        /// <summary>
+        /// Edit existing post in the database
+        /// </summary>
+        /// <param name="post">Updated post</param>
+        Task EditPost(Post post);
+
+        /// <summary>
+        /// Find and delete post by id
+        /// </summary>
+        /// <param name="id">Id of the post to be deleted</param>
+        Task DeletePost(int id);
+    }
+}
