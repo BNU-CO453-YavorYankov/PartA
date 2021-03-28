4700d864dc796cf78573afc27ace5e38326ea2f6
yavaka <yavakasolutions@gmail.com>
2021-03-28 00:11:01 +0000

    Added Post model

---
+++ WebApps/Models/App04/Post.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApps.Models.App04
+{
+    using System;
+    
+    /// <summary>
+    /// Post model that keeps info for
+    /// its description, created on date and author
+    /// </summary>
+    public class Post
+    {
+        public Post() => this.CreatedOn = DateTime.Now.Date;
+
+        public int PostId { get; set; }
+        public string Content { get; set; }
+        public string PhotoUrl { get; set; }
+        public DateTime CreatedOn { get; private set; }
+        public string AuthorId { get; set; }
+        public User Author { get; set; }
+    }
+}
