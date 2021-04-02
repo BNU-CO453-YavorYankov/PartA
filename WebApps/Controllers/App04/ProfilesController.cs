3346e0ca72b511e5d28cfc60291c663ce8815e3e
yavaka <yavakasolutions@gmail.com>
2021-04-02 22:36:00 +0100

    Added profile page with controller and view.

---
+++ WebApps/Controllers/App04/ProfilesController.cs
@@ -0,0 +1,35 @@
+﻿namespace WebApps.Controllers.App04
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+    using WebApps.Models.App04;
+    using WebApps.Services.Posts;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The controller responsible for user profile
+    /// </summary>
+    public class ProfilesController : Controller
+    {
+        private UserManager<User> _userManager;
+        private IPostService _postService;
+
+        public ProfilesController(
+            UserManager<User> userManager,
+            IPostService postService)
+        {
+            this._userManager = userManager;
+            this._postService = postService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await this._userManager.GetUserAsync(User);
+
+            currentUser.Posts = this._postService.GetPostsByAuthorId(currentUser.Id).ToList();
+
+            return View(currentUser);
+        }
+    }
+}
