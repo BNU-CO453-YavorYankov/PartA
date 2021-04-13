f7c8089a8087522968537d32721fec0c207430d7
yavaka <yavakasolutions@gmail.com>
2021-04-13 19:25:48 +0100

    Added comment model.

---
+++ WebApps/Models/App04/Comment.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApps.Models.App04
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using static ModelConstants.Post;
+
+    public class Comment
+    {
+        public int CommentId { get; set; }
+
+        [MinLength(MIN_CONTENT_LENGTH), MaxLength(MAX_CONTENT_LENGTH)]
+        public string Content { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public Post PostId { get; set; }
+        //Commented post
+        public Post Post { get; set; }
+        
+        public string AuthorId { get; set; }
+        
+        public User Author { get; set; }
+    }
+}
