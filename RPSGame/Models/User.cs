68c4eabf40e2c8125152e69382fde6586148c844
yavaka <yavakasolutions@gmail.com>
2021-05-25 14:37:01 +0100

    Added User model

---
+++ RPSGame/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace RPSGame.Models
+{
+    using System.ComponentModel.DataAnnotations;
+  
+    /// <summary>
+    /// This user model keep information about 
+    /// user`s name and number of rounds
+    /// </summary>
+    public class User
+    {
+        [Required]
+        [StringLength(20, ErrorMessage = "Name is too long.")]
+        [MinLength(2, ErrorMessage = "Name cannot be less than 3 symbols long.")]
+        public string Name { get; set; }
+        [Range(1, 20, ErrorMessage = "Number of rounds cannot be less than 1 or more than 20.")]
+        public int Rounds { get; set; }
+    }
+}
