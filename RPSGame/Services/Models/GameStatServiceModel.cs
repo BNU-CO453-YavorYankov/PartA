345be9901ec63d1137f873cd9158d47ebc16ff42
yavaka <yavakasolutions@gmail.com>
2021-05-27 17:36:13 +0100

    Added GameStatServiceModel

---
+++ RPSGame/Services/Models/GameStatServiceModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RPSGame.Services.Models
+{
+    /// <summary>
+    /// This model pass player`s name and the result of a given game.
+    /// It is used between GameService and XMLService.
+    /// </summary>
+    public class GameStatServiceModel
+    {
+        public string PlayerName { get; set; }
+        public string Result { get; set; }
+    }
+}
