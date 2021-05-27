295a0c70d167544115c6bccbe7958e368a9c8d92
yavaka <yavakasolutions@gmail.com>
2021-05-27 19:47:23 +0100

    Added GamesStat page.

--- RPSGame/Services/XMLService.cs
+++ RPSGame/Services/XMLService.cs
@@ -1,7 +1,10 @@
 ﻿using RPSGame.Services.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace RPSGame.Services
 {
@@ -14,25 +17,30 @@ namespace RPSGame.Services
         private const string GAMES_STAT_NODE = "games_stat";
         private const string GAME_NODE = "game";
         private const string PLAYER_NAME_NODE = "player_name";
-        private const string ROUNDS_WON_NODE = "rounds_won";
         private const string RESULT_NODE = "result";
 
+        /// <summary>
+        /// Full path to Data folder.
+        /// </summary>
         private string _dataFolderPath;
-
+        private string _xmlFile;
         public XMLService()
         {
             this._dataFolderPath = Path.GetFullPath($"{Environment.CurrentDirectory}/Data");
+            this._xmlFile = $"{this._dataFolderPath}\\games_stat.xml";
         }
 
+        /// <summary>
+        /// Create new XML with game stat if there is no existing
+        /// </summary>
+        /// <param name="model">last game stat</param>
         public void WriteXMLFile(GameStatServiceModel model)
         {
-            var xmlFile = $"{this._dataFolderPath}/games_stat.xml";
-
             XmlDocument doc = new XmlDocument();
 
-            if (File.Exists(xmlFile))
+            if (File.Exists(this._xmlFile))
             {
-                doc.Load(xmlFile);
+                doc.Load(this._xmlFile);
 
                 // <game>
                 XmlElement gameElement = doc.CreateElement(string.Empty, GAME_NODE, string.Empty);
@@ -73,7 +81,38 @@ namespace RPSGame.Services
                 gameElement.AppendChild(resultElement);
             }
 
-            doc.Save(xmlFile);
+            doc.Save(this._xmlFile);
+        }
+
+        /// <summary>
+        /// Load the XML file if there is so,
+        /// select game elements,
+        /// then go through each game element and 
+        /// add new key value pair from its data.
+        /// </summary>
+        public List<KeyValuePair<string, string>> ReadXML()
+        {
+            // Due to a dictionary cannot has duplicate keys, 
+            // we use list of type key value pair 
+            var results = new List<KeyValuePair<string, string>>();
+
+            if (File.Exists(this._xmlFile))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(this._xmlFile);
+
+                var xmlList = doc.SelectNodes("/games_stat/game");
+                foreach (XmlNode game in xmlList)
+                {
+                    string player_name = game["player_name"].InnerText;
+                    string result = game["result"].InnerText;
+
+                    results.Add(
+                        new KeyValuePair<string, string>(player_name, result));
+                }
+            }
+
+            return results;
         }
     }
 }
